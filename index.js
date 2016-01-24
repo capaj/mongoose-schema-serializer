73e3fac35de6b9dfe72c8b6698a9f22f106c561d
Jiri Spac <capajj@gmail.com>
2016-01-24 17:23:55 +0100

    all schema types supported now

--- index.js
+++ index.js
@@ -8,25 +8,39 @@ if (typeof window !== 'undefined') {
   glob = global
 }
 
-function stringify (schema) {
-  return JSON.stringify(schema, (key, value) => {
-    if (typeof value === 'function') {
-      return value.name
+module.exports = function (mgSchema) {
+  if (!mgSchema) {
+    mgSchema = {
+      types: {
+        Mixed: 'Mixed',
+        ObjectId: 'ObjectId'
+      }
     }
-    return value
-  })
-}
+  }
+  function stringify (schema) {
+    return JSON.stringify(schema, (key, value) => {
+      if (typeof value === 'function') {
+        return value.name
+      }
+      return value
+    })
+  }
 
-function parse (json) {
-  return JSON.parse(json, (key, value) => {
-    if (glob.hasOwnProperty(value)) {
-      return glob[value]
-    }
-    return value
-  })
-}
+  function parse (json) {
+    return JSON.parse(json, (key, value) => {
+      if (glob.hasOwnProperty(value)) {
+        return glob[value]
+      } else if (value === 'Mixed') {
+        return mgSchema.Types.Mixed
+      } else if (value === 'ObjectId') {
+        return mgSchema.Types.ObjectId
+      }
+      return value
+    })
+  }
 
-module.exports = {
-  stringify,
-  parse
+  return {
+    stringify,
+    parse
+  }
 }
--- test/serializer.spec.js
+++ test/serializer.spec.js
@@ -1,40 +1,67 @@
+/* eslint-env node, mocha */
 'use strict'
-const mss = require('../index')
 const expect = require('chai').expect
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+const mss = require('../index')(Schema)
 
 describe('serialization', function () {
   describe('should be able to serialize and deserialize all mongoose types', function () {
-    it.skip('string', function () {
+    it('string', function () {
       const schema = {
         string: String
       }
       const json = mss.stringify(schema)
       expect(json).to.equal('{"string":"String"}')
       expect(mss.parse(json)).to.eql(schema)
-
     })
     it('number', function () {
-      const schema = mss.stringify({
+      const schema = {
         num: Number
-      })
+      }
       const json = mss.stringify(schema)
       expect(json).to.equal('{"num":"Number"}')
       expect(mss.parse(json)).to.eql(schema)
     })
     it('date', function () {
-
+      const schema = {
+        num: Date
+      }
+      const json = mss.stringify(schema)
+      expect(json).to.equal('{"num":"Date"}')
+      expect(mss.parse(json)).to.eql(schema)
     })
     it('buffer', function () {
-
+      const schema = {
+        num: Buffer
+      }
+      const json = mss.stringify(schema)
+      expect(json).to.equal('{"num":"Buffer"}')
+      expect(mss.parse(json)).to.eql(schema)
     })
     it('boolean', function () {
-
+      const schema = {
+        num: Boolean
+      }
+      const json = mss.stringify(schema)
+      expect(json).to.equal('{"num":"Boolean"}')
+      expect(mss.parse(json)).to.eql(schema)
     })
     it('mixed', function () {
-
+      const schema = {
+        mixed: Schema.Types.Mixed
+      }
+      const json = mss.stringify(schema)
+      expect(json).to.equal('{"mixed":"Mixed"}')
+      expect(mss.parse(json)).to.eql(schema)
     })
     it('objectId', function () {
-
+      const schema = {
+        id: Schema.Types.ObjectId
+      }
+      const json = mss.stringify(schema)
+      expect(json).to.equal('{"id":"ObjectId"}')
+      expect(mss.parse(json)).to.eql(schema)
     })
   })
 })
